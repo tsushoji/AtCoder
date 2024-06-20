142edb3981870615e1e5c469ab247b4b94947f35
DESKTOP-2CCMS6G\ShotaTsuji <s.tuji3007sho@gmail.com>
2024-06-21 01:20:29 +0900

    C#用の補助ツール追加

---
+++ 0001_CSharp/SupportTool/SupportToolConsoleApp/Template/UnitTestMethod_001.cs
@@ -0,0 +1,9 @@
+        [TestMethod()]
+        [Timeout(2000)]
+        public void Test(TESTNUM)()
+        {
+            string input  = "(TESTCASEINPUT)";
+            string output = "(TESTCASEOUTPUT)";
+
+            AssertIO(input, output);
+        }
---
+++ 0001_CSharp/SupportTool/SupportToolConsoleApp/TestCaseSourceBuilderForAtcoder/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace TestCaseSourceBuilderForAtcoder
+{
+    internal class Program
+    {
+        private static Encoding _encoding = Encoding.GetEncoding("utf-8");
+        private static String _exeFolderPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        public static void Main(string[] args)
+        {
+            // AtCoaderの問題URLパラメータ
+            string targetURL = args[0].ToString();
+            // テストコード出力パスパラメータ
+            string outputPath = args[1].ToString();
+            if (targetURL == string.Empty)
+            {
+                Console.WriteLine("AtCoaderの問題URLパラメータが渡されておりません。");
+                return;
+            }
+            if (outputPath == string.Empty)
+            {
+                Console.WriteLine("テストコード出力パスパラメータが渡されておりません。");
+                return;
+            }
+
+            //テストコード出力
+            string testcases = GetTestCase(GetContext(targetURL.Trim(new char[] { '\r', '\n', ' ' })));
+            using (StreamWriter sw = new StreamWriter(outputPath, false, _encoding))
+            {
+                sw.Write(testcases);
+            }
+
+            Console.WriteLine("成功 出力テストコードパス:" + outputPath);
+        }
+
+        private static string GetTestCase(string html)
+        {
+            var testCasesStr = File.ReadAllText(_exeFolderPath + @"\\Template\\UnitTest_001.cs", _encoding);
+            var testCaseMethodStr = File.ReadAllText(_exeFolderPath + @"\\Template\\UnitTestMethod_001.cs", _encoding);
+
+            string anchor = "(<pre class=\"prettyprint linenums\">|<pre>)(?<testcase>.*?)</pre>";
+            Regex re = new Regex(anchor, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            StringBuilder sb = new StringBuilder();
+            int i = 1;
+            for (Match m = re.Match(html); m.Success; m = m.NextMatch())
+            {
+                if (m.Groups["testcase"].Value.Contains("<var>")) continue;
+
+                string testCaseInput = m.Groups["testcase"].Value; m = m.NextMatch();
+                string testCaseOutput = m.Groups["testcase"].Value;
+
+                testCaseInput = testCaseInput.TrimStart('\r').TrimStart('\n').Replace("\r", "").Replace("\n", "\\n");
+                testCaseOutput = testCaseOutput.TrimStart('\r').TrimStart('\n').TrimEnd('\n').TrimEnd('\r').Replace("\r", "").Replace("\n", "\\n");
+
+                sb.AppendLine(testCaseMethodStr.Replace("(TESTNUM)", (i++).ToString()).Replace("(TESTCASEINPUT)", testCaseInput).Replace("(TESTCASEOUTPUT)", testCaseOutput));
+            }
+
+            return testCasesStr.Replace("(TESTCASES)", sb.ToString());
+        }
+
+        private static string GetContext(string url)
+        {
+            WebClient wc = new WebClient();
+            byte[] data = wc.DownloadData(url);
+            Encoding enc = Encoding.GetEncoding("utf-8");
+            string ret = enc.GetString(data);
+            wc.Dispose();
+            return ret;
+        }
+    }
+}
